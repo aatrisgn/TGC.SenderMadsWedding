8a62c7bc39e5ca5e5b00ddffdfaab1b7e1b904d7
Asger Thyregod <asger.thyregod@gmail.com>
2025-01-12 20:22:19 +0100

    Outcommented code changes to web app and deploying DNS

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,3 +24,9 @@ resource "azurerm_static_web_app" "primary_static_web_app" {
   sku_tier            = "Free"
   sku_size            = "Free"
 }
+
+resource "azurerm_dns_zone" "dev-dns-zone" {
+  name                = local.dns_zone_resource_name
+  resource_group_name = data.azurerm_resource_group.predefined_resource_group.name
+}
+
