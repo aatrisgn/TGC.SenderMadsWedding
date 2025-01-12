1504c479605e319a974508dc31d5c5a89edd85e1
Asger Thyregod <asger.thyregod@gmail.com>
2025-01-12 21:45:30 +0100

    trying to deploy managed domain to Azure

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,3 +46,19 @@ resource "azurerm_dns_ns_record" "dev_childzone_record" {
 
   records = data.azurerm_dns_ns_record.dev_dns_zone[0].records //Not the prettiest, but easy solution since we will only have 1 DNS zone on this reference ever.
 }
+
+resource "azurerm_static_web_app_custom_domain" "example" {
+  static_web_app_id = azurerm_static_web_app.primary_static_web_app.id
+  domain_name       = var.environment_type_name == "dev" ? "dev.madsogsender210625.dk" : "madsogsender210625.dk"
+  validation_type   = "dns-txt-token"
+}
+
+resource "azurerm_dns_txt_record" "example" {
+  name                = var.environment_type_name == "dev" ? "_dnsauth.dev" : "_dnsauth"
+  zone_name           = var.environment_type_name == "dev" ? "dev.madsogsender210625.dk" : "madsogsender210625.dk"
+  resource_group_name = data.azurerm_resource_group.predefined_resource_group.name
+  ttl                 = 300
+  record {
+    value = azurerm_static_web_app_custom_domain.example.validation_token
+  }
+}
