6c96322791ea40186c3860d51fc18835d7b2aff5
Asger Thyregod <asger.thyregod@gmail.com>
2025-01-12 14:19:24 +0100

    Cleaned up and started on infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,7 @@
+
+
+resource "azurerm_static_web_app" "example" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+}
