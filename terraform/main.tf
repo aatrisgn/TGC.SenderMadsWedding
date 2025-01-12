7c76768a616be5af367577b4a763bc4cf4638fa7
Asger Thyregod <asger.thyregod@gmail.com>
2025-01-12 17:55:45 +0100

    Ensured dependency and fixed SKU issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,6 @@ resource "azurerm_static_web_app" "primary_static_web_app" {
   name                = "swa-sendermadswedding-${var.environment_type_name}-${local.resource_location_name}"
   resource_group_name = data.azurerm_resource_group.predefined_resource_group.name
   location            = data.azurerm_resource_group.predefined_resource_group.location
-  sku_tier            = "free"
-  sku_size            = "free"
+  sku_tier            = "Free"
+  sku_size            = "Free"
 }
